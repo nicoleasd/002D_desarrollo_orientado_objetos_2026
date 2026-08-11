862daffd025b56d529b6a867e3e4eccf2747263f
nicoleasd <nicaguilar2006@gmail.com>
2026-08-11 17:05:31 -0400

    ejercicio clase1_DO

---
+++ src/Perro.java
@@ -0,0 +1,12 @@
+public class Perro {
+        String nombre;
+        String raza;
+        int edad;
+        double peso;
+        boolean vacuna;
+
+
+        public String ladrar(){
+            return "Guauuu Guauu";
+        }
+}
---
+++ src/PerroSinPoo.java
@@ -0,0 +1,29 @@
+public class PerroSinPoo {
+    
+
+    public static void main(String[] args) {
+        String nombre = "Cachupin";
+        String raza = "Quiltro";
+        int edad = 4;
+        double peso = 22.3;
+        boolean vacuna = true;
+
+        System.out.println("Perros tonotos!¡");
+
+        System.out.println("El nombre del perro tonoto es:");
+        System.out.println(nombre);
+
+        System.out.println("La raza del perro tonoto es:");
+        System.out.println(raza);
+
+        System.out.println("La edad del perro tonoto es:");
+        System.out.println(edad);
+
+        System.out.println("El peso del perro tonoto es:");
+        System.out.println(peso);
+
+        System.out.println("El perro tonoto esta vacunado: ");
+        System.out.println(vacuna);
+        
+    }
+}
