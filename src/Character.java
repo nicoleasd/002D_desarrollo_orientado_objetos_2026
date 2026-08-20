0dd8525c64739614b6d0a212b14923ad6f35b9f0
nicoleasd <nicaguilar2006@gmail.com>
2026-08-20 18:44:47 -0400

    clase 4

--- src/Character.java
+++ src/Character.java
@@ -3,10 +3,24 @@ public class Character {
     private int hit_points;
     int magic_points;
     boolean is_ally;
+    private int age;
 
 
+    public void validarEdad(int age){
+        if (age > 0){
+            System.out.println("EL programa lo dejo en 0");
+            this.age = 0;
+        }else{
+            this.age = age;
+        }
+    }
+       
+    public int mostrarEdad(){
+        return this.age ;
+    }
+//----------------------------------------------
     public void asignarVida(int hit_points){
-        if (hit_points < 0 ){
+        if (hit_points < 0 ) {
             this.hit_points = 0;
         
         }else{
@@ -16,5 +30,7 @@ public class Character {
     public int mostrarVida(){
         return this.hit_points;
     }
+//--------------------------------------------
+        //agregar validaciones de nombres, para qno sean insultos
+    }
 
-}
--- src/Main.java
+++ src/Main.java
@@ -14,46 +14,44 @@ public class Main {
 
         boolean flag_menu = true;
 
-        System.out.println("*****Bienvenido a los 7 reinos*****");
+        System.out.println("*****Welcome to the seven kingdoms*****");
 
         while (flag_menu) {
-        System.out.println("1. Create Character");
-        System.out.println("2. Inspect Character");
-        System.out.println("3. Visualize Character");
-        System.out.println("3. Exit");
+            System.out.println("1. Create Character");
+            System.out.println("2. Inspect Character");
+            System.out.println("3. Visualize Character");
+            System.out.println("3. Exit");
 
-        System.out.println("choose an option");
+            System.out.println("choose an option");
 
-        String user_option = scanner.nextLine();
+            String user_option = scanner.nextLine();
+
 
         switch (user_option) {
             case "1": 
             System.out.println("Creating Character");
             
-
-            System.out.println("Ingrese un nombre: ");    
+            System.out.println("Enter a nickname: ");    
             player_character.name = scanner.nextLine();
 
-            System.out.println("INgrese puntos de vida: ");
-            //player_character.hit_points = Integer.parseInt(scanner.nextLine());
+            System.out.println("Add Hp: ");
+            player_character.asignarVida(Integer.parseInt(scanner.nextLine()));
 
-
-            
-            
-            System.out.println("INgrese maná");
+            System.out.println("Add Magic Points to ur character: ");
             player_character.magic_points = Integer.parseInt(scanner.nextLine());
-
+        
+            System.out.println("Enter an age: ");
+            player_character.validarEdad(Integer.parseInt(scanner.nextLine()));
                 break;
+
 
             case "2":
                 System.out.println("ver persnaje");
 
-                System.out.println("NOMBRE: " + player_character.name);
-                System.out.println("HP " + player_character.hit_points);
-                System.out.println("MANÁ " + player_character.magic_points);
-
-            
-
+                System.out.println("NAME: " + player_character.name);
+                System.out.println("HP: " + player_character.mostrarVida());
+                System.out.println("MP: " + player_character.magic_points);
+                System.out.println("AGE: " + player_character.mostrarEdad());
 
                 break;
         
@@ -63,7 +61,7 @@ public class Main {
                 break;
 
             default:
-                System.out.println("opcion invalida");
+                System.out.println("Invalid Option");
                 break;
         }
 
