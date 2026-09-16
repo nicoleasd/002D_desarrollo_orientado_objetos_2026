f13e17b0917a64cf1ce2aaf483bf86c2765698c8
nicoleasd <nicaguilar2006@gmail.com>
2026-09-16 18:50:35 -0300

    agregue lo basico del menu, aun faltan mas opciones

--- src/Tienda.java
+++ src/Tienda.java
@@ -26,6 +26,137 @@ public class Tienda {
             System.out.println("7. Agregar datos de prueba");
 
             String opcion_menu = sc.nextLine();
+
+            switch (opcion_menu) {
+                case "1":
+                    registrarProducto();
+                    break;
+
+                case "2":
+                    listarInventario();
+                    break;
+
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+                case "5":
+                    break;
+
+
+                case "6":
+                    mostrar_menu = false;
+                    break;
+                
+                    
+                case "7":
+                    break;
+
+            
+                default:
+                    System.out.println("opcion invalida");
+                    break;
+            }
+
+
+
         }
     }
+
+
+    static void  registrarProducto() {
+
+        boolean mostrar_submenu = true;
+
+        while (mostrar_submenu) {
+
+        System.out.println(" --- Tipo de Producto --- ");
+        System.out.println("1. Físico");
+        System.out.println("2. Digital");
+        System.out.println("3. Volver al menú");
+        System.out.println("Selecciones una opción:");
+
+        String opcion_submenu = sc.nextLine();
+
+        switch (opcion_submenu) {
+            case "1":
+                registrarProductoFisico();
+                break;
+            
+            case "2":
+                registrarProductoDigital();
+                break;
+
+            case "3":
+                mostrar_submenu = false;
+
+                break;
+
+
+            default:
+                System.out.println("Opción inválida.");
+                break;
+            }
+        } 
+    }
+
+    static void registrarProductoFisico(){
+        System.out.println("Nombre de juego: ");
+        String nombre_ingresado = sc.nextLine();
+
+        System.out.println("Precio Base: ");
+        int precio_base = Integer.parseInt(sc.nextLine()); //el integer.parseint tranforma el texto a numero
+                                                           // falta agregarle manejo de exepciones, por si alguien coloca "abc"
+        System.out.println("Stock: ");
+        int stock = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Costo de envío: ");
+        int costo_envio = Integer.parseInt(sc.nextLine());
+
+
+        ProductoFisico juego = new ProductoFisico(nombre_ingresado, precio_base, stock, costo_envio);
+        
+        if (lista_juegos_fisicos.add(juego)) {
+            System.out.println("Producto físico registrado.");
+
+        } else {
+            System.out.println("No se registró el producto digital.");
+        }
+    }
+
+    static void registrarProductoDigital(){          //misma logica q el productoFisico
+        System.out.println("Nombre de juego: ");
+        String nombre = sc.nextLine();
+
+        System.out.println("Precio Base: ");
+        int precio_base = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Stock: ");
+        int stock = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Decuento: ");
+        int descuento = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Plataforma: ");
+        String plataforma =  sc.nextLine();
+
+        ProductoDigital juegoD = new ProductoDigital(nombre, precio_base, stock, descuento, plataforma);
+
+        if (lista_juegos_digitales.add(juegoD)) {
+            System.out.println("Producto digital registrado.");
+            
+        } else {
+            System.out.println("No se registró el producto degital.");
+        }
+
+    }
+
+    static void listarInventario(){
+        System.out.println("-_-_-_- INVENTARIO DE JUEGOS -_-_-_-");
+        
+    }
+
+
 }
